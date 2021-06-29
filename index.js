8bfebb8d39392ebc27e7b3fceecd20e07f06e984
Prince <avecillasprince@yahoo.com>
2021-06-29 17:30:06 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`    // used ` b/c of ${}
+}
+
+function mondayWork(x='go to the office') {
+    return `This Monday, I will ${x}.`
+}
+
+let wrapAdjective = function(x="*") {
+    return function(y="special") {
+        return `You are ${x}${y}${x}!`
+    }
+
+}
